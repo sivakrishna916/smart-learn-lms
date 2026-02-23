0fad424f63c40bbc29f91c941904b4b2f60655fc
sivakrishna916 <152765733+sivakrishna916@users.noreply.github.com>
2026-02-23 11:32:52 +0530

    Delete Secure-Learning-Management-System-main directory

--- Secure-Learning-Management-System-main/lms-client/hash-admin.js
+++
@@ -1,2 +0,0 @@
-const bcrypt = require('bcryptjs');
-bcrypt.hash('admin12345', 10).then(console.log);
--- Secure-Learning-Management-System-main/lms-client/src/routes/PrivateRoute.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
-import { useAuth } from '../context/AuthContext';
-
-export default function PrivateRoute() {
-  const { user, loading } = useAuth();
-  if (loading) return null;
-  return user ? <Outlet /> : <Navigate to="/login" replace />;
-} 
